d96353c97a8ca29c85787c219cfcd4e0a15e49e3
NickKirby20 <kirbynic000@warren.k12.in.us>
2021-09-16 12:49:05 -0400

    TrianglePerimeter

--- L3-Return Methods/src/TrianglePerimeter.java
+++ L3-Return Methods/src/TrianglePerimeter.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class TrianglePerimeter {
 
     /*
@@ -10,7 +13,60 @@ public class TrianglePerimeter {
     for calculating the perimeter of a triangle.
      */
 
+     /*
+       1.Get inputs - coordinates, x1,y1,x2,y2.x3,y3
+       2.calculate distances - 1->2 ,2->3 3->1
+       3.calculate perimeter- side1 + side2 + side3
+       4.Display results/outputs = round to 2 decimals
+     */
+
+
     public static void main(String[] args) {
 
+        int x1 = getInput("point 1, x:");
+        int y1 = getInput("point 1, y:");
+
+        int x2 = getInput("point 2, x:");
+        int y2 = getInput("point 2, y:");
+
+        int x3 = getInput("point 3, x:");
+        int y3 = getInput("point 3, y:");
+
+
+        double distance1 = distance(x1, y1, x2, y2);
+        double distance2 = distance(x2, y2, x3, y3);
+        double distance3 = distance(x1, y1, x3, y3);
+
+        double perimeter = perimeter(distance1, distance2,distance3);
+
+        output(perimeter);
+
     }
+
+    public static int getInput(String prompt){
+
+        return Integer.parseInt(JOptionPane.showInputDialog(prompt));
+
+    }
+
+
+     public static double distance(int x1, int y1, int x2, int y2){
+
+         return Math.sqrt(Math.pow(x2 - x1, 2)) + Math.pow(y2-y1, 2);
+
+    }
+
+    public static double perimeter(double side1, double side2, double side3){
+
+        return side1 + side2 + side3;
+    }
+
+    public static void output(double perimeter){
+    DecimalFormat round = new DecimalFormat("#,###.00");
+        JOptionPane.showMessageDialog(null, "The perimeter is " + perimeter);
+
+    }
+
+
+
 }
